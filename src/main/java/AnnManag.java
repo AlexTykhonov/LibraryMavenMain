96fa1c174994f30d1dc616d2c7cded61f4949fcb
AlexTykhonov <alexey.tykhonov@gmail.com>
2021-02-19 20:32:05 +0200

    Updated library calculator: - returns annuity payment - returns approximate amount of standard loan limit based on payment, rate, period. - returns approximate amount of annity loan limit based on payment, rate and period.

--- src/main/java/AnnManag.java
+++ src/main/java/AnnManag.java
@@ -4,10 +4,16 @@ public class AnnManag {
 
     public static void main(String[] args) {
 
+        // тест расчета платежа по аннуитетному кредиту
         Annuitator annuitator = new Annuitator();
-        System.out.println(annuitator.annPayment(12,12,100));
+//        System.out.println(annuitator.annPayment(12,12,100));
 
-        System.out.println("NEAREST LIMIT IS "+annuitator.getAmountStnd(405,123,60));
+        // тест процесса поиска суммы кредита под сумму платежа и ставку и число периодов - СТАНДАРТНЫЙ ГРАФИК
+//        System.out.println("NEAREST LIMIT IS "+annuitator.getAmountStnd(405,123,60));
+
+        // тест процесса поиска суммы кредита под сумму платежа и ставку и число периодов - АННУИТЕТ
+        System.out.println("NEAREST ANNUITY LIMIT IS "+annuitator.getAmountAn(202,12,12));
+
 
     }
 }
--- src/main/java/Modules/Annuitator.java
+++ src/main/java/Modules/Annuitator.java
@@ -39,8 +39,8 @@ public class Annuitator {
         this.amount = amount;
     }
 
-    public double annPayment(double p, double r, double amount) {
-        return amount * (r / 12 / 100) / (1 - (Math.pow((1 + r / 12 / 100), -p)));
+    public double annPayment(double periods, double rate, double amount) {
+        return amount * (rate / 12 / 100) / (1 - (Math.pow((1 + rate / 12 / 100), -periods)));
     }
 
 
@@ -50,18 +50,31 @@ public class Annuitator {
 
         while (distance < 0) {
 
-
             distance = (limit / periodSt) + (limit * rateSt / 1200) - paymentSt;
             System.out.println("Limit" + limit);
             System.out.println("DISTANCE " + distance);
             System.out.println("Init payment " + paymentSt);
             System.out.println("Current payment " + (limit / periodSt) + (limit * rateSt / 1200));
             limit = limit + 10;
-
         }
-
         return limit;
-
-
     }
+
+    public double getAmountAn(double paymentAn, double rateAn, int periodAn) {
+        double limit = paymentAn;
+        double distance = -1;
+
+        while (distance < 0) {
+
+            distance = annPayment(periodAn,rateAn,limit) - paymentAn;
+            System.out.println("Limit" + limit);
+            System.out.println("DISTANCE " + distance);
+            System.out.println("Init payment " + paymentAn);
+            System.out.println("Current payment " + (limit / periodAn) + (limit * rateAn / 1200));
+            limit = limit + 10;
+        }
+        return limit;
+    }
+
+
 }
